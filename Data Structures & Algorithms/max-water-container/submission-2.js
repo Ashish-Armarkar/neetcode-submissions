4120d8d9ee7e8450b2697d62eebed1d10cebfeff
Ashish Armarkar <85281482+Ashish-Armarkar@users.noreply.github.com>
2026-04-28 00:15:43 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let calc = 0;
+        let l = 0;
+        let r = heights.length-1;
+        while(r>l){
+            let diff =r-l
+            let sm = heights[l] > heights[r] ? heights[r] : heights[l]
+            let newCalc = sm * diff
+            if(newCalc > calc){
+                calc = newCalc
+            }else if(heights[l] > heights[r]){
+                r--
+            }else{
+                l++
+            }
+                
+
+        }
+        return calc;
+    }
+}
