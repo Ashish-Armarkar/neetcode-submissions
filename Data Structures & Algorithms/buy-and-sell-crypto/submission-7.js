3f5205faabcc9a7ead0962192aaf0e0eb9abe582
Ashish Armarkar <85281482+Ashish-Armarkar@users.noreply.github.com>
2026-05-04 22:34:40 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(p) {
+        let l = 0;
+        let nxt = 1;
+        let maxP = 0;
+        while(nxt<=p.length-1){
+            if(p[l]>p[nxt]){
+                l = nxt
+                nxt++
+            }else {
+                if(maxP < p[nxt] - p[l]) maxP = p[nxt] - p[l];
+                nxt++;
+            }
+        }
+        return maxP
+    }
+}
